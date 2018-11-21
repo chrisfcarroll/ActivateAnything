6012994c6a50e34b4ec18679d17cb8eedac79404
Chris F carroll <chris.carroll@unforgettable.me.uk>
2018-11-21 23:25:11 +0000

    TestBase.FixtureBase passes tests

--- TestBase.FixtureBase.Tests/FixtureBaseExample.cs
+++ TestBase.FixtureBase.Tests/FixtureBaseExample.cs
@@ -1,6 +1,10 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Dapper;
 using TestBase.AdoNet;
 using Xunit;
 
@@ -10,11 +14,32 @@ namespace TestBase.FixtureBase.Specs
     public class FixtureBaseExample : FixtureBaseWithDbAndHttpFor<AUseCase>
     {
         [Fact]
-        public void UUTreturnsDataFromDb()
+        public void UUTreturnsDataFromDbQuerySingleColumn()
         {
-            var dbData = new[] { (1,"row1"), (2,"row2"), (3,"row3")};
-            Db.SetUpForQuery(dbData);
-            UnitUnderTest.FromDb().ShouldEqualByValue(dbData);
+            var dbData = new[] { "row1", "row2", "row3", "row4"};
+            Db.QueueCommand(FakeDbCommand.ForExecuteSingleColumnQuery(dbData));
+            UnitUnderTest.FromDbStrings().ShouldEqualByValue(dbData);
+        }
+
+        [Fact]
+        public void UUTreturnsDataFromDbQuery()
+        {
+            var dataToReturn = new[]
+            {
+                new IdAndName {Id = 11, Name = "cell 1,2"}, 
+                new IdAndName {Id = 21, Name = "cell 2,2"}
+            };
+            Db.SetUpForQuery(dataToReturn,new[] {"Id", "Name"});
+            UnitUnderTest
+                .FromDbIdAndNames()
+                .ShouldEqualByValue(dataToReturn);
+        }
+
+        [Fact]
+        public void UUTreturnsDataFromDbQueryScalar()
+        {
+            Db.SetUpForQueryScalar(999);
+            UnitUnderTest.FromDb().ShouldBeOfLength(1).First().ShouldBe(999);
         }
 
         [Fact]
@@ -30,4 +55,62 @@ namespace TestBase.FixtureBase.Specs
         }
 
     }
+
+    public class IdAndName{ public int Id { get;set; } public string Name { get; set; }}
+
+    public class AUseCase
+    {
+        readonly IServiceRequiringDBandHttp service;
+
+        public AUseCase(IServiceRequiringDBandHttp service)
+        {
+            this.service = service;
+        }
+
+        public List<int> FromDb() => service.GetFromDb();
+        public List<IdAndName> FromDbIdAndNames() => service.GetFromDbIdAndNames();
+        public async Task<string> FromHttpService() => await service.GetFromHttp();
+        public List<string> FromDbStrings() => service.GetFromDbStrings();
+    }
+
+    public interface IServiceRequiringDBandHttp
+    {
+        List<IdAndName> GetFromDbIdAndNames();
+        List<int> GetFromDb();
+        Task<string> GetFromHttp();
+        List<string> GetFromDbStrings();
+    }
+
+    class AServiceRequiringDBandHttp : IServiceRequiringDBandHttp
+    {
+        readonly IDbConnection db;
+        readonly System.Net.Http.HttpClient httpClient;
+
+        public AServiceRequiringDBandHttp(IDbConnection db, System.Net.Http.HttpClient httpClient)
+        {
+            this.db = db;
+            this.httpClient = httpClient;
+        }
+
+        public List<IdAndName> GetFromDbIdAndNames()
+        {
+            return db.Query<IdAndName>("Select * Fom AClass").ToList();
+        }
+
+        public List<int> GetFromDb()
+        {
+            return db.Query<int>("Select * Fom AClass").ToList();
+        }
+
+        public async Task<string> GetFromHttp()
+        {
+            return await httpClient.GetStringAsync("http://localhost/string");
+        }
+
+        public List<string> GetFromDbStrings()
+        {
+            return db.Query<string>("Select * Fom AClass").ToList();
+        }
+    }
+
 }
--- TestBase.FixtureBase.Tests/FixtureBaseSpecs.cs
+++ TestBase.FixtureBase.Tests/FixtureBaseSpecs.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using ActivateAnything;
-using Dapper;
 using TestBase.AdoNet;
 using TestBase.HttpClient.Fake;
 using Xunit;
@@ -53,46 +48,5 @@ namespace TestBase.FixtureBase.Specs
         [Fact]public void HttpClientShouldNotBeNull() => HttpClient.ShouldNotBeNull();
         [Fact]public void DbShouldBeFakeDb() => Db.ShouldBeIn(Instances).ShouldBeOfType<FakeDbConnection>();
         [Fact]public void HttpClientShouldBeFakeHttpClient() => HttpClient.ShouldBeIn(Instances).ShouldBeOfType<FakeHttpClient>();
-    }
-
-    public class AUseCase
-    {
-        readonly IServiceRequiringDBandHttp service;
-
-        public AUseCase(IServiceRequiringDBandHttp service)
-        {
-            this.service = service;
-        }
-
-        public List<string> FromDb() => service.GetFromDb();
-        public async Task<string> FromHttpService() => await service.GetFromHttp();
-    }
-
-    public interface IServiceRequiringDBandHttp
-    {
-        List<string> GetFromDb();
-        Task<string> GetFromHttp();
-    }
-
-    class AServiceRequiringDBandHttp : IServiceRequiringDBandHttp
-    {
-        readonly IDbConnection db;
-        readonly System.Net.Http.HttpClient httpClient;
-
-        public AServiceRequiringDBandHttp(IDbConnection db, System.Net.Http.HttpClient httpClient)
-        {
-            this.db = db;
-            this.httpClient = httpClient;
-        }
-
-        public List<string> GetFromDb()
-        {
-            return db.Query<string>("Select * Fom AClass").ToList();
-        }
-
-        public async Task<string> GetFromHttp()
-        {
-            return await httpClient.GetStringAsync("http://localhost/string");
-        }
     }
 }
